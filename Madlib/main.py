5964d37c3f499ddd3c92dc5defae80cc3ad09e93
kylegarrettwilson <LetItBe@c-67-201-233-162.reshall.wwu.edu>
2016-01-07 16:42:53 -0800

    2 of 6
    I added one array and one dictionary because I wanted to work from the
    top down using my raw inputs. Meaning I wanted to start with strings
    and then work into numbers

--- Madlib/main.py
+++ Madlib/main.py
@@ -16,7 +16,7 @@ customer_name = raw_input("Enter your name:  ")
 
 car_type = raw_input("Enter the vehicle you wish to drive:  ")
 
-car_color = raw_input("Enter the color you wish the vehicle to be:  ")
+car_color = raw_input("Do you want a white or red vehicle:  ")
 
 passenger_count = raw_input("Enter the number of passengers (including yourself):  ")
 
@@ -26,3 +26,23 @@ years_driving = raw_input("Enter the number of years you have had a license:  ")
 
 
 print "Hello and welcome " + customer_name + ". Thank you for choosing Rad Rentals."
+
+
+# one array
+
+cars = ["Cobalt", "Nova", "Mustang"]
+cars.append(car_type)
+print "The vehicles we have availabe are ", cars
+
+
+
+# one dictionary
+
+color = dict()
+color = {"white":"granny", "red":"speed freak"}
+print "The color you chose says a lot about who you are. You must be a", color[car_color]
+
+
+
+
+
