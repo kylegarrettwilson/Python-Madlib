7a99862d53e5ea17815eec1d5aadfa756002f16e
kylegarrettwilson <LetItBe@c-67-201-245-154.reshall.wwu.edu>
2016-01-14 18:18:22 -0800

    self.request and self.response
    I finished the requests and responses so it accepts the data from the
    form and then writes it for us in a new page. Now to make it look pretty

--- simple-login/main.py
+++ simple-login/main.py
@@ -16,16 +16,12 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
         page_head = '''<!DOCTYPE HTML>
-<html
+<html>
     <head>
     <title></title>
 
-
-
-    <style>
-
-    </style>
 
 
     </head>
@@ -35,39 +31,49 @@ class MainHandler(webapp2.RequestHandler):
 
 
 
-    page_body = '''<form method="GET">
-        <label>First Name: </label><input type="text" name="first" />
-        <label>Last Name: </label><input type="text" name="last" />
-        <label>Email: </label><input type="email" name="email" />
-        <input type="radio" name="gender" value="male" checked>Male
-        <br>
-        <input type="radio" name="gender" value="female">Female
-        <br>
-        <input type="radio" name="experience" value="yes" checked>I have a currently have a job
-        <br>
-        <input type="radio" name="experience" value="no">I do not currently have a job
-        <br>
-        <select name="education">
-            <option value="doctoral">I have a Phd</option>
-            <option value="masters">I have a Masters degree</option>
-            <option value="bachelors">I have a Bachelors degree</option>
-            <option value="associates">I have an Associates degree</option>
-            <option value="nodegree">I do not have a college degree</option>
-        </select>
-        <label>How many days out of the week can you work?</label><br>
-        <input type="number" name="quantity" min="1" max="5">
-        <input type="submit" value="Submit" />'''
+        page_body = '''<form method="GET">
+            <label>First Name: </label><input type="text" name="first" />
+            <label>Last Name: </label><input type="text" name="last" />
+            <label>Email: </label><input type="email" name="email" />
+            <input type="radio" name="gender" value="male" checked>Male
+            <br>
+            <input type="radio" name="gender" value="female">Female
+            <br>
+            <input type="radio" name="experience" value="yes" checked>I have a currently have a job
+            <br>
+            <input type="radio" name="experience" value="no">I do not currently have a job
+            <br>
+            <select name="education">
+                <option value="doctoral">I have a Phd</option>
+                <option value="masters">I have a Masters degree</option>
+                <option value="bachelors">I have a Bachelors degree</option>
+                <option value="associates">I have an Associates degree</option>
+                <option value="nodegree">I do not have a college degree</option>
+            </select>
+            <label>How many days out of the week can you work?</label><br>
+            <input type="number" name="quantity" min="1" max="5">
+            <input type="submit" value="Submit" />'''
 
 
 
 
-    page_close = '''
+        page_close = '''
         </form>
     </body>
 </html>'''
 
 
-
+        if self.request.GET:
+            first = self.request.GET['first']
+            last = self.request.GET['last']
+            email = self.request.GET['email']
+            radio = self.request.GET['gender']
+            check = self.request.GET['experience']
+            select = self.request.GET['education']
+            number = self.request.GET['quantity']
+            self.response.write(page_head + first + ' ' + last + ' ' + email + ' ' + radio + ' ' + check + ' ' + select + ' ' + number + ' ' + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close)
 
 
 
