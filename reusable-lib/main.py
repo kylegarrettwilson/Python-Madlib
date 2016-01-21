45af3b72bdad1d60c1996451599866f0b6e6d60c
kylegarrettwilson <LetItBe@c-67-201-245-154.reshall.wwu.edu>
2016-01-21 15:49:26 -0800

    setting up form
    I decided to start out by focusing on the form that will intake the
    data. I started by creating a form page class

--- reusable-lib/main.py
+++ reusable-lib/main.py
@@ -10,13 +10,41 @@
 
 
 
-
-
 import webapp2
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- reusable-lib/page.py
+++ reusable-lib/page.py
@@ -3,3 +3,17 @@
 # second class is ResultsPage and this will display the information after it has been calculated
 # the HTML needs to be in view classes
 # at least four input from the user
+
+
+class FormPage(object):  # this is the page class for holding the html elements
+    def __init__(self):  # constructor method
+        self.title = "Welcome!"  # this is a variable that changes the self.title in html
+        self.css = "css/style.css"  # this is a link to the css file from the yaml
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
