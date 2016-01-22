28571a4df4f1662880a0d789f91be1983f34146b
kylegarrettwilson <LetItBe@c-67-201-245-154.reshall.wwu.edu>
2016-01-21 16:40:58 -0800

    Job data object
    I am setting up the job data object to hold the info that will later be
    messed with, now to do some encapsulation with it

--- reusable-lib/library.py
+++ reusable-lib/library.py
@@ -1,3 +1,13 @@
 # this will hold the classes that will interpret and calculate the data
 # needs to be fully encapsulated
 # three utility functions
+
+
+
+class JobData(object):
+    def __init__(self):
+
+        self.name = ''
+        self.hours = 0
+        self.pay = 0
+        self.over = 0
