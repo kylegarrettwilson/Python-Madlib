dfa83a767e3e6e03ad070a526a88e7fb7cc46a41
kylegarrettwilson <LetItBe@c-67-201-232-44.reshall.wwu.edu>
2016-01-29 16:33:46 -0800

    Updating data objects
    I now have the five employee files that I can now use for the website

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -24,37 +24,37 @@ class Data(object):
         carl.email = "carl@waterman.com"
         carl.phone = "541-990-8987"
 
-        carl = Employee()
-        carl.name = "Carl Filtz"
-        carl.title = "Senior Accountant"
-        carl.image = "carl.jpg"
-        carl.bio = "Carl has been the lead accountant for 10 years."
-        carl.email = "carl@waterman.com"
-        carl.phone = "541-990-8987"
+        roy = Employee()
+        roy.name = "Roy Barnes"
+        roy.title = "Junior Accountant"
+        roy.image = "roy.jpg"
+        roy.bio = "Roy has been a junior accountant for 2 years."
+        roy.email = "roy@waterman.com"
+        roy.phone = "541-930-7773"
 
-        carl = Employee()
-        carl.name = "Carl Filtz"
-        carl.title = "Senior Accountant"
-        carl.image = "carl.jpg"
-        carl.bio = "Carl has been the lead accountant for 10 years."
-        carl.email = "carl@waterman.com"
-        carl.phone = "541-990-8987"
+        ned = Employee()
+        ned.name = "Ned Lighthouse"
+        ned.title = "Junior Accountant"
+        ned.image = "ned.jpg"
+        ned.bio = "Ned has been a junior accountant for 4 years."
+        ned.email = "ned@waterman.com"
+        ned.phone = "541-960-9292"
 
-        carl = Employee()
-        carl.name = "Carl Filtz"
-        carl.title = "Senior Accountant"
-        carl.image = "carl.jpg"
-        carl.bio = "Carl has been the lead accountant for 10 years."
-        carl.email = "carl@waterman.com"
-        carl.phone = "541-990-8987"
+        pete = Employee()
+        pete.name = "Pete Kohlman"
+        pete.title = "Junior Accountant"
+        pete.image = "pete.jpg"
+        pete.bio = "Pete has been a junior accountant for 3 years."
+        pete.email = "carl@waterman.com"
+        pete.phone = "541-990-8987"
 
-        carl = Employee()
-        carl.name = "Carl Filtz"
-        carl.title = "Senior Accountant"
-        carl.image = "carl.jpg"
-        carl.bio = "Carl has been the lead accountant for 10 years."
-        carl.email = "carl@waterman.com"
-        carl.phone = "541-990-8987"
+        heather = Employee()
+        heather.name = "Heather Jones"
+        heather.title = "Tax Lawyer"
+        heather.image = "heather.jpg"
+        heather.bio = "Heather is an attorney who specializes in tax law."
+        heather.email = "heather@waterman.com"
+        heather.phone = "541-340-9983"
 
 
 
