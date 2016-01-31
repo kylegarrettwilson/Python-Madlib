ad41af2716a7ceafbb39a2d8b2c958adb8fbf939
kylegarrettwilson <LetItBe@c-67-201-232-44.reshall.wwu.edu>
2016-01-30 18:32:44 -0800

    comments
    I am adding the comments for the data page and making sure there is no
    extra leftover code like last week’s assignment

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -4,7 +4,7 @@
 
 
 
-class Employee(object):
+class Employee(object):    # this is employee function that will work with the main handler on each employees data
     def __init__(self):
         self.name = ""
         self.title = ""
@@ -14,16 +14,16 @@ class Employee(object):
         self.phone = ""
         self.wage = 0
 
-    def calc_wage(self):
+    def calc_wage(self):  # we needed to have on calculation in our project and I thought price per appointment would be good to know
         total = self.wage * 4   # price per hour times by four hours for average meeting time
-        return str(total)
+        return str(total)  # return total
 
 
 class Data(object):
     def __init__(self):
 
-        carl = Employee()
-        carl.name = "Carl Filtz"
+        carl = Employee()   # invoking employee class
+        carl.name = "Carl Filtz"  # these are all values for the employee attributes above for each specific employee
         carl.title = "Senior Accountant"
         carl.image = "images/carl.jpg"
         carl.bio = "Carl has been the lead accountant for 10 years."
@@ -31,8 +31,8 @@ class Data(object):
         carl.phone = "541-990-8987"
         carl.wage = 70
 
-        roy = Employee()
-        roy.name = "Roy Barnes"
+        roy = Employee() # invoking employee class
+        roy.name = "Roy Barnes" # these are all values for the employee attributes above for each specific employee
         roy.title = "Junior Accountant"
         roy.image = "images/roy.jpg"
         roy.bio = "Roy has been a junior accountant for 2 years."
@@ -40,8 +40,8 @@ class Data(object):
         roy.phone = "541-930-7773"
         roy.wage = 34
 
-        ned = Employee()
-        ned.name = "Ned Lighthouse"
+        ned = Employee() # invoking employee class
+        ned.name = "Ned Lighthouse" # these are all values for the employee attributes above for each specific employee
         ned.title = "Junior Accountant"
         ned.image = "images/ned.jpg"
         ned.bio = "Ned has been a junior accountant for 4 years."
@@ -49,8 +49,8 @@ class Data(object):
         ned.phone = "541-960-9292"
         ned.wage = 40
 
-        pete = Employee()
-        pete.name = "Pete Kohlman"
+        pete = Employee()  # invoking employee class
+        pete.name = "Pete Kohlman" # these are all values for the employee attributes above for each specific employee
         pete.title = "Junior Accountant"
         pete.image = "images/pete.jpg"
         pete.bio = "Pete has been a junior accountant for 3 years."
@@ -58,8 +58,8 @@ class Data(object):
         pete.phone = "541-990-8987"
         pete.wage = 38
 
-        heather = Employee()
-        heather.name = "Heather Jones"
+        heather = Employee()  # invoking employee class
+        heather.name = "Heather Jones" # these are all values for the employee attributes above for each specific employee
         heather.title = "Tax Lawyer"
         heather.image = "images/heather.jpg"
         heather.bio = "Heather is an attorney who specializes in tax law."
@@ -67,5 +67,5 @@ class Data(object):
         heather.phone = "541-340-9983"
         heather.wage = 230
 
-        self.people = [carl, roy, ned, pete, heather]
+        self.people = [carl, roy, ned, pete, heather]  #placing the data above into an array to access later
 
